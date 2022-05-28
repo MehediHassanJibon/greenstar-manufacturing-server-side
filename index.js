4865b5845f40ab4ab809f26c773a3f368c3e7828
Mehedi Hassan Jibon <mhj124177@gmail.com>
2022-05-28 22:19:58 +0600

    dynamic route for payment id

--- index.js
+++ index.js
@@ -75,7 +75,12 @@ app.get('/order/:email', async (req, res) => {
   const order = await orderCollection.find(filter).toArray();
   res.send(order.reverse());
 });
-
+app.get('/payment/:id', async (req, res) => {
+  const id = req.params.id;
+  const filter = { _id: ObjectId(id) }
+  const order = await orderCollection.findOne(filter);
+  res.send(order);
+});
   } finally {
       //   await client.close();
   }
