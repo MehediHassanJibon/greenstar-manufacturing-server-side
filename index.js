80b1d3ec567ecabcbbfad24462bccbe0091a0619
Mehedi Hassan Jibon <mhj124177@gmail.com>
2022-05-28 22:18:38 +0600

    dynamic route for order mail

--- index.js
+++ index.js
@@ -69,6 +69,12 @@ app.delete('/deleteOrder/:id', async (req, res) => {
   const result = await orderCollection.deleteOne(filter);
   res.send(result);
 })
+app.get('/order/:email', async (req, res) => {
+  const email = req.params.email;
+  const filter = { email: email }
+  const order = await orderCollection.find(filter).toArray();
+  res.send(order.reverse());
+});
 
   } finally {
       //   await client.close();
